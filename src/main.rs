589bbe267d786e135d3fe4f5a5d4f2d4f8886c52
Nerdling-S <nerdlings9@gmail.com>
2023-11-07 22:41:04 +0200

    winit: got a window up and running

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use winit;
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = match winit::event_loop::EventLoop::new() {
+        Ok(lp) => lp,
+        Err(_) => std::process::exit(0)
+    };
+    let window = winit::window::WindowBuilder::new()
+        .with_inner_size(winit::dpi::LogicalSize::new(600,400))
+        .with_title("opengl-winit")
+        .with_enabled_buttons(winit::window::WindowButtons::CLOSE)
+        .build(&event_loop);
+
+    event_loop.run(|event, elwt| {
+        match event {
+            winit::event::Event::WindowEvent {
+                event: winit::event::WindowEvent::CloseRequested, ..
+            } => elwt.exit(),
+            _ => ()
+        }
+    }).unwrap();
 }
