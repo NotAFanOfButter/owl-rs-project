e6fef07f30f6f5b14594292c901dfd7fdd11d43d
Nerdling-S <nerdlings9@gmail.com>
2023-11-10 15:08:55 +0200

    MOTHERFUCKING TRIANGLE!!!

--- src/main.rs
+++ src/main.rs
@@ -26,6 +26,63 @@ fn main() {
     }
     gl::load_with(|symbol| context.get_proc_address(symbol) as *const _);
 
+    let vertices: [f32; 9] = [
+        0.0, -0.5, 0.0,
+        -0.5, 0.5, 0.0,
+        0.5, 0.5, 0.0
+    ];
+    let mut vao = 0;
+    let mut vbo = 0;
+    unsafe {
+        gl::GenVertexArrays(1, &mut vao);
+        assert_ne!(vao,0);
+        gl::GenBuffers(1, &mut vbo);
+        assert_ne!(vbo, 0);
+    
+        gl::BindVertexArray(vao);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(gl::ARRAY_BUFFER,
+            std::mem::size_of_val(&vertices) as isize,
+            vertices.as_ptr().cast(),
+            gl::STATIC_DRAW);
+        gl::VertexAttribPointer(0, 3, gl::FLOAT, gl::FALSE,
+            (3 * std::mem::size_of::<f32>()).try_into().unwrap(),
+            0 as *const std::ffi::c_void);
+        gl::EnableVertexAttribArray(0);
+    }
+    let vertex_shader = unsafe { gl::CreateShader(gl::VERTEX_SHADER) };
+    unsafe {
+        let vertex_shader_source: std::ffi::CString = std::ffi::CString::new(r#"
+            #version 430 core
+            layout (location = 0) in vec3 aPos;
+
+            void main() {
+                gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+            }
+        "#).expect("Failed to convert shader source to c-string: vertex");
+        gl::ShaderSource(vertex_shader, 1, &vertex_shader_source.as_ptr(), std::ptr::null());
+        gl::CompileShader(vertex_shader);
+    }
+    let fragment_shader = unsafe { gl::CreateShader(gl::FRAGMENT_SHADER) };
+    unsafe {
+        let fragment_shader_source = std::ffi::CString::new(r#"
+            #version 430 core
+            out vec4 fragColour;
+
+            void main() {
+                fragColour = vec4(1.0, 0.0, 0.0, 1.0);
+            }
+        "#).expect("Failed to covert shader source to c-string: fragment");
+        gl::ShaderSource(fragment_shader, 1, &fragment_shader_source.as_ptr(), std::ptr::null());
+        gl::CompileShader(fragment_shader);
+    }
+    let shader_program = unsafe { gl::CreateProgram() };
+    unsafe {
+        gl::AttachShader(shader_program, vertex_shader);
+        gl::AttachShader(shader_program, fragment_shader);
+        gl::LinkProgram(shader_program);
+    }
+
     event_loop.run(|event, elwt| {
         match event {
             Event::WindowEvent {event, ..} => {
@@ -35,6 +92,9 @@ fn main() {
                         unsafe {
                             gl::ClearColor(0.0, 0.0, 0.0, 1.0);
                             gl::Clear(gl::COLOR_BUFFER_BIT);
+                            gl::UseProgram(shader_program);
+                            gl::BindVertexArray(vao);
+                            gl::DrawArrays(gl::TRIANGLES, 0, 3);
                         }
                         context.swap_buffers();
                     },
@@ -44,4 +104,10 @@ fn main() {
             _ => ()
         }
     }).expect("Failed to run an event loop");
+
+    unsafe {
+        gl::DeleteShader(vertex_shader);
+        gl::DeleteShader(fragment_shader);
+        gl::DeleteProgram(shader_program);
+    }
 }
