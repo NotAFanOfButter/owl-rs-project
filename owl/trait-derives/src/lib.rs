1b8cf35bd725bbb30babef154a70f6f299143c91
huginn <46073986+NotAFanOfButter@users.noreplay.github.com>
2024-01-10 18:48:52 +0200

    working trait derive for ToVecByte

--- owl/trait-derives/src/lib.rs
+++ owl/trait-derives/src/lib.rs
@@ -1,7 +1,7 @@
 use proc_macro::TokenStream;
 use proc_macro2;
-use syn;
 use quote::quote;
+use syn;
 
 #[proc_macro_derive(ToByteVec)]
 pub fn to_byte_vec_derive(input: TokenStream) -> TokenStream {
@@ -14,38 +14,47 @@ pub fn to_byte_vec_derive(input: TokenStream) -> TokenStream {
     if let syn::Data::Struct(data_struct) = data {
         let where_clause = &generics.where_clause;
         let to_byte_stream = parse_fields_to_bytes(data_struct);
-        quote!{
+        quote! {
             impl #generics ToByteVec for #ident #generics #where_clause {
                 fn to_byte_vec(self) -> Vec<u8> {
                     #to_byte_stream
                 }
             }
-        }.into()
+        }
+        .into()
     } else {
         quote!().into()
     }
 }
 
 fn parse_fields_to_bytes(data: syn::DataStruct) -> proc_macro2::TokenStream {
-    let fields = match data.fields {
-        syn::Fields::Named(named) => named.named,
-        syn::Fields::Unnamed(unnamed) => unnamed.unnamed,
-        syn::Fields::Unit => syn::punctuated::Punctuated::new()
-    };
+    // let fields = match data.fields {
+    //     syn::Fields::Named(named) => named.named,
+    //     syn::Fields::Unnamed(unnamed) => unnamed.unnamed,
+    //     syn::Fields::Unit => syn::punctuated::Punctuated::new(),
+    // };
     if let syn::Fields::Named(named) = data.fields {
         let named_fields = named.named;
-        let built = quote!(let mut bytes = Vec:new(););
-        for field in fields {
+        let mut built = quote!(let mut bytes = Vec::new(););
+        for field in named_fields {
             let ident = field.ident;
-            built.extend(quote!{
+            built.extend(quote! {
                 bytes.extend(self.#ident.to_byte_vec());
             })
         }
         built.extend(quote!(bytes));
-        built
+        built.into()
     } else if let syn::Fields::Unnamed(unnamed) = data.fields {
-        for (i,_) in unnamed.unnamed.iter().enumerate() {
-            
+        let mut built = quote!(let mut bytes = Vec::new(););
+        for (i, _) in unnamed.unnamed.iter().enumerate() {
+            let index = syn::Index::from(i);
+            built.extend(quote! {
+                bytes.extend(self.#index.to_byte_vec());
+            })
         }
+        built.extend(quote!(bytes));
+        built.into()
+    } else {
+        quote!(self.0.to_byte_vec()).into()
     }
 }
