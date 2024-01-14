79a92a20d28b52932f2887e77e2ef7ddf7438587
huginn <46073986+NotAFanOfButter@users.noreplay.github.com>
2024-01-14 19:23:05 +0200

    added custom error message to `OwlError` for other errors that do not originate from the bindings

--- owl/src/errors.rs
+++ owl/src/errors.rs
@@ -2,20 +2,41 @@ use crate::ox;
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct OwlError {
     info: String,
-    internal: ox::OxError
+    internal: Option<ox::OxError>
+}
+impl OwlError {
+    pub(crate) fn custom(message: &str) -> Self {
+        OwlError { info: message.to_owned(), internal: None }
+    }
 }
 
 impl std::fmt::Display for OwlError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f,"{}: {}", self.info, self.internal)
+        match self.internal {
+            Some(ref internal) => write!(f,"{}: {}", self.info, internal),
+            None => write!(f, "{}: Other", self.info),
+        }
+        
     }
 }
+
 impl std::error::Error for OwlError {}
+
 pub(crate) trait UnmessagedError {
-    fn with_message(self, message: &str) -> OwlError;
+    type MessagedError;
+    fn with_message(self, message: &str) -> Self::MessagedError;
 }
+
 impl UnmessagedError for ox::OxError {
+    type MessagedError = OwlError;
     fn with_message(self, message: &str) -> OwlError {
-        OwlError { internal: self, info: message.to_owned() }
+        OwlError { internal: Some(self), info: message.to_owned() }
+    }
+}
+
+impl<T> UnmessagedError for Result<T, ox::OxError> {
+    type MessagedError = Result<T, OwlError>;
+    fn with_message(self, message: &str) -> Result<T, OwlError> {
+        self.map_err(|e| e.with_message(message))
     }
 }
