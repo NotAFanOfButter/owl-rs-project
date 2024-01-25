dd59debb98847f00fb558ce1c717fc24fbb39b1d
huginn <46073986+NotAFanOfButter@users.noreplay.github.com>
2024-01-25 17:16:28 +0200

    changed error system, to only forward source in Display, not internally, reworked `UnmessagedError` into `ToOwlError` allowing for better message passing and implementation on (Result<T,)OwlError(>)

--- owl/src/errors.rs
+++ owl/src/errors.rs
@@ -1,46 +1,91 @@
 use crate::ox;
+#[allow(unused_imports)] // for use in other mods only
+pub(crate) use crate::safe_bindings::Error as OriginalError;
+
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct OwlError {
-    info: String,
-    internal: Option<ox::OxError>
+    context: String,
+    error_message: String,
 }
 impl OwlError {
     pub(crate) fn custom(message: &str) -> Self {
-        Self { info: message.to_owned(), internal: None }
-    }
-    pub(crate) fn with_context(self, context: &str) -> Self {
-        Self { info: format!("{context}, {}", self.info), ..self }
+        Self { error_message: message.to_owned(), context: String::new() }
     }
 }
 
 impl std::fmt::Display for OwlError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self.internal {
-            Some(ref internal) => write!(f,"{}: {}", self.info, internal),
-            None => write!(f, "{}: Other", self.info),
-        }
-        
+        write!(f, "{}: {}", self.context, self.error_message)        
     }
 }
 
 impl std::error::Error for OwlError {}
 
-#[allow(clippy::redundant_pub_crate)] // emphasise should not be used externally, despite currently private mod
-pub(crate) trait UnmessagedError {
-    type MessagedError;
-    fn with_message(self, message: &str) -> Self::MessagedError;
+pub(crate) trait ToOwlError {
+    type OwlErrorType;
+    fn with_context(self, context: &str) -> Self::OwlErrorType;
+    fn with_message(self, message: &str) -> Self::OwlErrorType;
+    /// Avoid most of the time, unless the original OpenGL message is actually useful by itself
+    fn no_message(self) -> Self::OwlErrorType;
 }
 
-impl UnmessagedError for ox::OxError {
-    type MessagedError = OwlError;
-    fn with_message(self, message: &str) -> OwlError {
-        OwlError { internal: Some(self), info: message.to_owned() }
+impl ToOwlError for ox::OxError {
+    type OwlErrorType = OwlError;
+    fn with_context(self, context: &str) -> OwlError {
+        OwlError { context: context.to_owned(), error_message: format!("{self}") }
+    }
+    fn with_message(self, message: &str) -> Self::OwlErrorType {
+        OwlError { context: String::new(), error_message: format!("{message}, {self}") }
+    }
+    fn no_message(self) -> Self::OwlErrorType {
+        OwlError { context: String::new(), error_message: String::new() }
     }
 }
 
-impl<T> UnmessagedError for Result<T, ox::OxError> {
-    type MessagedError = Result<T, OwlError>;
+impl<T> ToOwlError for Result<T, ox::OxError> {
+    type OwlErrorType = Result<T, OwlError>;
+    fn with_context(self, context: &str) -> Result<T, OwlError> {
+        self.map_err(|e| e.with_context(context))
+    }
     fn with_message(self, message: &str) -> Result<T, OwlError> {
         self.map_err(|e| e.with_message(message))
+    }
+    fn no_message(self) -> Self::OwlErrorType {
+        Err(OwlError { context: String::new(), error_message: String::new() })
+    }
+}
+
+impl ToOwlError for OwlError {
+    type OwlErrorType = Self;
+    fn with_context(self, context: &str) -> Self {
+        let context = if self.context.is_empty() {
+            context.to_owned()
+        } else {
+            format!("{context}, {}", self.context)
+        };
+        Self { context, ..self }
+    }
+    /// Replaces the previous message
+    fn with_message(self, message: &str) -> Self {
+        Self { error_message: message.to_owned(), ..self }
+    }
+    /// why you would use this, I have no idea
+    fn no_message(self) -> Self::OwlErrorType {
+        Self { context: String::new(), error_message: String::new() }
+    }
+}
+
+impl<T> ToOwlError for Result<T,OwlError> {
+    type OwlErrorType = Self;
+    fn with_context(self, context: &str) -> Self {
+        self.map_err(|e| e.with_context(context))
+    }
+    /// Replaces the previous message
+    fn with_message(self, message: &str) -> Self {
+        self.map_err(|e| e.with_message(message))
+    }
+    /// why you would use this, I have no idea
+    fn no_message(self) -> Self::OwlErrorType {
+        self.map_err(OwlError::no_message)
     }
 }
